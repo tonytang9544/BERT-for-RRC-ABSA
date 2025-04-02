1c195c8e32ba18d5e0a262a3f1aac5642d1191e4
tonytang9544 <tonytang9544@gmail.com>
2025-04-03 00:04:13 +0100

    added new data_utils for preparing dataset. Not yet done.

---
+++ pytorch-pretrained-bert/src/data_utils.py
@@ -0,0 +1,21 @@
+# Copyright 2018 The Google AI Language Team Authors and The HuggingFace Inc. team and authors from University of Illinois at Chicago.
+# Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+    
+import json
+
+
+def read_squad_examples(input_file, is_training=True):
+    with open(input_file, "r", encoding='utf-8') as reader:
+        input_data = json.load(reader)["data"]
